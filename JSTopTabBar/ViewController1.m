feda8d637d7cc49b7a2bf301a529c6dc956065ec
Josh Sklar <jsklar@detroitlabs.com>
2014-01-04 22:02:50 -0500

    refactored
    changed button names; added highlight title color; added method declarations

--- JSTopTabBar/ViewController1.m
+++ JSTopTabBar/ViewController1.m
@@ -11,6 +11,8 @@
 
 @interface ViewController1 ()
 
+- (void)didTapToggleButton:(UIButton*)sender;
+- (void)didTapToggleEnableBorder:(UIButton*)sender;
 - (void)didTapToggleShadow:(UIButton*)sender;
 
 @end
@@ -26,33 +28,37 @@
     [l setText:@"This is an empty view controller."];
     [self.view addSubview:l];
     
+    static const CGFloat btnHeight = 50;
+    
     UIButton *toggle = [[UIButton alloc]
-                        initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 50)];
+                        initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, btnHeight)];
     [toggle setTitle:@"Toggle JSTTBC"
             forState:UIControlStateNormal];
     [toggle addTarget:self
                action:@selector(didTapToggleButton:)
      forControlEvents:UIControlEventTouchUpInside];
     
-    UIButton *enableBorder = [[UIButton alloc]
-                              initWithFrame:CGRectMake(0, 170, self.view.frame.size.width, 50)];
-    [enableBorder setTitle:@"Toggle JSTTBC button border"
+    UIButton *toggleBorders = [[UIButton alloc]
+                              initWithFrame:CGRectMake(0, 160, self.view.frame.size.width, btnHeight)];
+    [toggleBorders setTitle:@"Toggle JSTTBC button borders"
                   forState:UIControlStateNormal];
-    [enableBorder addTarget:self
+    [toggleBorders addTarget:self
                      action:@selector(didTapToggleEnableBorder:)
            forControlEvents:UIControlEventTouchUpInside];
     
     UIButton *toggleShadow = [[UIButton alloc]
-                              initWithFrame:CGRectMake(0, 230, self.view.frame.size.width, 50)];
+                              initWithFrame:CGRectMake(0, 220, self.view.frame.size.width, btnHeight)];
     [toggleShadow setTitle:@"Toggle JSTTBC button shadow"
                   forState:UIControlStateNormal];
     [toggleShadow addTarget:self
                      action:@selector(didTapToggleShadow:)
            forControlEvents:UIControlEventTouchUpInside];
     
-    for (UIButton *b in @[toggle, enableBorder, toggleShadow]) {
+    for (UIButton *b in @[toggle, toggleBorders, toggleShadow]) {
         [b setTitleColor:[UIColor blueColor]
                      forState:UIControlStateNormal];
+        [b setTitleColor:[UIColor grayColor]
+                forState:UIControlStateHighlighted];
         [b setBackgroundColor:[UIColor yellowColor]];
         [self.view addSubview:b];
     }
