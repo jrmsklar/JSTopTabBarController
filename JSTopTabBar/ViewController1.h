de063afadb85e49275a9d1e3c0c7561fa18a4ffd
Josh Sklar <jrmsklar@gmail.com>
2015-01-06 02:09:08 -0500

    use xib for ViewController1

--- JSTopTabBar/ViewController1.h
+++ JSTopTabBar/ViewController1.h
@@ -1,14 +1,13 @@
 //
-//  RegularViewController.h
+//  ViewController1.h
 //  JSTopTabBar
 //
-//  Created by Josh Sklar on 5/14/13.
-//  Copyright (c) 2013 Josh Sklar. All rights reserved.
+//  Created by Josh Sklar on 1/6/15.
+//  Copyright (c) 2015 Josh Sklar. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController1 : UIViewController
-
 
 @end
--- JSTopTabBar/ViewController1.m
+++ JSTopTabBar/ViewController1.m
@@ -1,9 +1,9 @@
 //
-//  RegularViewController.m
+//  ViewController1.m
 //  JSTopTabBar
 //
-//  Created by Josh Sklar on 5/14/13.
-//  Copyright (c) 2013 Josh Sklar. All rights reserved.
+//  Created by Josh Sklar on 1/6/15.
+//  Copyright (c) 2015 Josh Sklar. All rights reserved.
 //
 
 #import "ViewController1.h"
@@ -11,91 +11,32 @@
 
 @interface ViewController1 ()
 
-- (void)didTapToggleButton:(UIButton*)sender;
-- (void)didTapToggleEnableBorder:(UIButton*)sender;
-- (void)didTapToggleShadow:(UIButton*)sender;
-
 @end
 
 @implementation ViewController1
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
-    UILabel *l = [[UILabel alloc]initWithFrame:CGRectMake(0, 40, self.view.frame.size.width, 50)];
-    [l setTextAlignment:NSTextAlignmentCenter];
-    [l setText:@"This is an empty view controller."];
-    [self.view addSubview:l];
-    
-    static const CGFloat btnHeight = 50;
-    
-    // TODO: Move all these to a xib file.
-    
-    UIButton *toggle = [[UIButton alloc]
-                        initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, btnHeight)];
-    [toggle setTitle:@"Toggle JSTTBC"
-            forState:UIControlStateNormal];
-    [toggle addTarget:self
-               action:@selector(didTapToggleButton:)
-     forControlEvents:UIControlEventTouchUpInside];
-    
-    UIButton *toggleBorders = [[UIButton alloc]
-                              initWithFrame:CGRectMake(0, 160, self.view.frame.size.width, btnHeight)];
-    [toggleBorders setTitle:@"Toggle JSTTBC button borders"
-                  forState:UIControlStateNormal];
-    [toggleBorders addTarget:self
-                     action:@selector(didTapToggleEnableBorder:)
-           forControlEvents:UIControlEventTouchUpInside];
-    
-    UIButton *toggleShadow = [[UIButton alloc]
-                              initWithFrame:CGRectMake(0, 220, self.view.frame.size.width, btnHeight)];
-    [toggleShadow setTitle:@"Toggle JSTTBC button shadow"
-                  forState:UIControlStateNormal];
-    [toggleShadow addTarget:self
-                     action:@selector(didTapToggleShadow:)
-           forControlEvents:UIControlEventTouchUpInside];
-    
-    UIButton *togglePanning = [[UIButton alloc]
-                               initWithFrame:CGRectMake(0, 280, CGRectGetWidth(self.view.frame), btnHeight)];
-    [togglePanning setTitle:@"Toggle JSTTBC arrow button panning"
-                   forState:UIControlStateNormal];
-    [togglePanning addTarget:self
-                      action:@selector(didTapTogglePanning:)
-            forControlEvents:UIControlEventTouchUpInside];
-    
-    for (UIButton *b in @[toggle, toggleBorders, toggleShadow, togglePanning]) {
-        [b setTitleColor:[UIColor blueColor]
-                     forState:UIControlStateNormal];
-        [b setTitleColor:[UIColor grayColor]
-                forState:UIControlStateHighlighted];
-        [b setBackgroundColor:[UIColor yellowColor]];
-        [self.view addSubview:b];
-    }
-}
-
 #pragma mark - Internal methods
 
-- (void)didTapToggleButton:(UIButton*)sender
+- (IBAction)didTapToggleButton:(UIButton*)sender
 {
     [self.topTabBar performToggleTopTabBar];
 }
 
-- (void)didTapToggleEnableBorder:(UIButton*)sender
+- (IBAction)didTapToggleEnableBorder:(UIButton*)sender
 {
     static BOOL toggle = NO;
     [self.topTabBar enableBordersOnTopTabBarButtons:toggle];
     toggle = !toggle;
 }
 
-- (void)didTapToggleShadow:(UIButton*)sender
+- (IBAction)didTapToggleShadow:(UIButton*)sender
 {
     static BOOL toggleShadow = NO;
     [self.topTabBar enableShadowOnTopTabBarButton:toggleShadow];
     toggleShadow = !toggleShadow;
 }
 
-- (void)didTapTogglePanning:(UIButton*)sender
+- (IBAction)didTapTogglePanning:(UIButton*)sender
 {
     static BOOL togglePanning = YES;
     [self.topTabBar enablePanningOfToggleTopTabBarButton:togglePanning];
