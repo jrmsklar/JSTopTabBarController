cd1819d69359ea63fd84f147f5bdead8d09ec85b
Josh Sklar <jrmsklar@gmail.com>
2015-01-06 22:31:26 -0500

    use xib, simplify ViewController2 logic

--- JSTopTabBar/ViewController2.h
+++ JSTopTabBar/ViewController2.h
@@ -2,8 +2,8 @@
 //  ViewController2.h
 //  JSTopTabBar
 //
-//  Created by Josh Sklar on 5/14/13.
-//  Copyright (c) 2013 Josh Sklar. All rights reserved.
+//  Created by Josh Sklar on 1/6/15.
+//  Copyright (c) 2015 Josh Sklar. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- JSTopTabBar/ViewController2.m
+++ JSTopTabBar/ViewController2.m
@@ -2,12 +2,11 @@
 //  ViewController2.m
 //  JSTopTabBar
 //
-//  Created by Josh Sklar on 5/14/13.
-//  Copyright (c) 2013 Josh Sklar. All rights reserved.
+//  Created by Josh Sklar on 1/6/15.
+//  Copyright (c) 2015 Josh Sklar. All rights reserved.
 //
 
 #import "ViewController2.h"
-#import "JSTopTabBarController.h"
 
 @interface ViewController2 ()
 
@@ -15,37 +14,10 @@
 
 @implementation ViewController2
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.title = @"Nav Controller";
-    self.view.backgroundColor = [UIColor whiteColor];
-    UILabel *l = [[UILabel alloc]initWithFrame:self.view.frame];
-    [l setTextAlignment:NSTextAlignmentCenter];
-    [l setText:@"This is a navigation controller."];
-    [self.view addSubview:l];
-    
-	// Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
+    self.title = @"Navigation Controller";
 }
 
 @end
